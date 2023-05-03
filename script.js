ecb0a54668b81667d80aeaea3633a2c6836ac2ca
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-03 21:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+var browserName = navigator.appName;
+    var version = navigator.appVersion;
+    var message = "You are using " + browserName + " version " + version;
+document.querySelector("#browser-info").textcontext= message;
